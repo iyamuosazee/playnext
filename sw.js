cc9b2f40e813bd64336eeffd436a0c702dbbdf46
iyamuosazee <40112515+iyamuosazee@users.noreply.github.com>
2026-08-09 16:27:10 +0100

    Refresh PWA cache for V2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='playnext-v2';
+const CACHE='playnext-v2-1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
